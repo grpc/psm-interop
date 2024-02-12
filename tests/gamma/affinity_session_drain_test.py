fa56794a84f1bd2a846a67ae432e537a9dcb59fe
Sergii Tkachenko <sergiitk@google.com>
2024-02-12 14:03:26 -0800

    Move AffinitySessionDrainTest to own file

--- tests/gamma/affinity_session_drain_test.py
+++ tests/gamma/affinity_session_drain_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023 gRPC authors.
+# Copyright 2024 gRPC authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,15 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import datetime
 import logging
-from typing import List, Optional
+from typing import Optional
 
 from absl import flags
 from absl.testing import absltest
+from google.protobuf import json_format
 
 from framework import xds_gamma_testcase
 from framework import xds_k8s_testcase
 from framework import xds_url_map_testcase
+from framework.helpers import retryers
 from framework.rpc import grpc_testing
 from framework.test_app import client_app
 from framework.test_app import server_app
@@ -28,19 +31,26 @@ from framework.test_cases import session_affinity_util
 logger = logging.getLogger(__name__)
 flags.adopt_module_key_flags(xds_k8s_testcase)
 
+# Type aliases
 _XdsTestServer = server_app.XdsTestServer
 _XdsTestClient = client_app.XdsTestClient
 RpcTypeUnaryCall = xds_url_map_testcase.RpcTypeUnaryCall
 
+# Constants
 _REPLICA_COUNT = 3
-
-# TODO(rbellevi): set this property on the prestop hook test class
-# We never actually hit this timeout under normal circumstances, so this large
-# value is acceptable.
-# termination_grace_period_seconds: int = 600
+_TERMINATION_GRACE_PERIOD_SECONDS = 600
 
 
-class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
+class AffinitySessionDrainTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
+    # @override
+    def setUp(self):
+        self.pre_stop_hook = True
+        self.termination_grace_period_seconds = (
+            _TERMINATION_GRACE_PERIOD_SECONDS
+        )
+        super(AffinitySessionDrainTest, self).setUp()
+
+    # @override
     def getClientRpcStats(
         self,
         test_client: _XdsTestClient,
@@ -55,47 +65,7 @@ class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
             metadata_keys=metadata_keys or client_app.REQ_LB_STATS_METADATA_ALL,
         )
 
-    def test_session_affinity_filter(self):
-        test_servers: List[_XdsTestServer]
-        with self.subTest("01_run_test_server"):
-            test_servers = self.startTestServers(
-                replica_count=_REPLICA_COUNT,
-                route_template="gamma/route_http_ssafilter.yaml",
-            )
-
-        with self.subTest("02_create_ssa_filter"):
-            self.server_runner.createSessionAffinityFilter()
-
-        # Default is round robin LB policy.
-
-        with self.subTest("03_start_test_client"):
-            test_client: _XdsTestClient = self.startTestClient(test_servers[0])
-
-        with self.subTest("04_send_first_RPC_and_retrieve_cookie"):
-            (
-                cookie,
-                chosen_server,
-            ) = session_affinity_util.assert_eventually_retrieve_cookie_and_server(
-                self, test_client, test_servers
-            )
-
-        with self.subTest("05_send_RPCs_with_cookie"):
-            test_client.update_config.configure(
-                rpc_types=(RpcTypeUnaryCall,),
-                metadata=(
-                    (
-                        RpcTypeUnaryCall,
-                        "cookie",
-                        cookie,
-                    ),
-                ),
-            )
-            self.assertRpcsEventuallyGoToGivenServers(
-                test_client, [chosen_server], 10
-            )
-
-    def test_session_affinity_policy_with_route_target(self):
-        test_servers: List[_XdsTestServer]
+    def test_session_drain(self):
         with self.subTest("01_run_test_server"):
             test_servers = self.startTestServers(replica_count=_REPLICA_COUNT)
 
@@ -104,12 +74,15 @@ class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
                 "gamma/session_affinity_policy_route.yaml"
             )
 
+        with self.subTest("03_create_backend_policy"):
+            self.server_runner.createBackendPolicy()
+
         # Default is round robin LB policy.
 
-        with self.subTest("03_start_test_client"):
+        with self.subTest("04_start_test_client"):
             test_client: _XdsTestClient = self.startTestClient(test_servers[0])
 
-        with self.subTest("04_send_first_RPC_and_retrieve_cookie"):
+        with self.subTest("05_send_first_RPC_and_retrieve_cookie"):
             (
                 cookie,
                 chosen_server,
@@ -117,7 +90,7 @@ class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
                 self, test_client, test_servers
             )
 
-        with self.subTest("05_send_RPCs_with_cookie"):
+        with self.subTest("06_send_RPCs_with_cookie"):
             test_client.update_config.configure(
                 rpc_types=(RpcTypeUnaryCall,),
                 metadata=(
@@ -132,30 +105,45 @@ class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
                 test_client, [chosen_server], 10
             )
 
-    def test_session_affinity_policy_with_service_target(self):
-        test_servers: List[_XdsTestServer]
-        with self.subTest("01_run_test_server"):
-            test_servers = self.startTestServers(replica_count=_REPLICA_COUNT)
+        with self.subTest("07_initiate_backend_pod_termination"):
+            chosen_server.send_hook_request_start_server()
+            self.server_runner.delete_pod_async(chosen_server.hostname)
 
-        with self.subTest("02_create_ssa_policy"):
-            self.server_runner.createSessionAffinityPolicy(
-                "gamma/session_affinity_policy_service.yaml"
+        with self.subTest("08_confirm_backend_is_draining"):
+
+            def _assert_draining():
+                config = test_client.csds.fetch_client_status(
+                    log_level=logging.INFO
+                )
+                self.assertIsNotNone(config)
+                json_config = json_format.MessageToDict(config)
+                parsed = xds_url_map_testcase.DumpedXdsConfig(json_config)
+                logging.info("Received CSDS: %s", parsed)
+                self.assertLen(parsed.draining_endpoints, 1)
+
+            retryer = retryers.constant_retryer(
+                wait_fixed=datetime.timedelta(seconds=10),
+                attempts=3,
+                log_level=logging.INFO,
+            )
+            retryer(_assert_draining)
+
+        with self.subTest("09_send_RPCs_to_draining_server"):
+            self.assertRpcsEventuallyGoToGivenServers(
+                test_client, [chosen_server], 10
             )
 
-        # Default is round robin LB policy.
-
-        with self.subTest("03_start_test_client"):
-            test_client: _XdsTestClient = self.startTestClient(test_servers[0])
-
-        with self.subTest("04_send_first_RPC_and_retrieve_cookie"):
+        with self.subTest("10_kill_old_server_and_receive_new_assignment"):
+            chosen_server.send_hook_request_return()
+            refreshed_servers = self.refreshTestServers()
             (
                 cookie,
-                chosen_server,
+                new_chosen_server,
             ) = session_affinity_util.assert_eventually_retrieve_cookie_and_server(
-                self, test_client, test_servers
+                self, test_client, refreshed_servers
             )
 
-        with self.subTest("05_send_RPCs_with_cookie"):
+        with self.subTest("11_send_traffic_to_new_assignment"):
             test_client.update_config.configure(
                 rpc_types=(RpcTypeUnaryCall,),
                 metadata=(
@@ -167,7 +155,7 @@ class AffinityTest(xds_gamma_testcase.GammaXdsKubernetesTestCase):
                 ),
             )
             self.assertRpcsEventuallyGoToGivenServers(
-                test_client, [chosen_server], 10
+                test_client, [new_chosen_server], 10
             )
 
 
