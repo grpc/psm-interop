6ac09d9d0b52ecab36983f2d5c27914606165356
Stanley Cheung <stanleycheung@google.com>
2024-04-18 23:24:27 +0000

    reran black.sh

--- tests/app_net_csm_observability_test.py
+++ tests/app_net_csm_observability_test.py
@@ -330,7 +330,7 @@ class AppNetCsmObservabilityTest(xds_k8s_testcase.AppNetXdsKubernetesTestCase):
                 "csm_remote_workload_project_id": self.project,
                 "csm_remote_workload_type": "gcp_kubernetes_engine",
                 "csm_service_name": ANY,  # TODO(yashykt)
-                "csm_service_namespace_name": 'unknown',
+                "csm_service_namespace_name": "unknown",
                 "csm_workload_canonical_service": CSM_CANONICAL_SERVICE_NAME_CLIENT,
                 "grpc_method": GRPC_METHOD_NAME,
                 "grpc_status": "OK",
