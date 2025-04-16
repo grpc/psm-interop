b913893fc9a129e6ffb6b50fcae407abd43bc36c
eshitachandwani <emchandwani@google.com>
2025-04-16 11:20:18 +0530

    retry

--- framework/xds_k8s_testcase.py
+++ framework/xds_k8s_testcase.py
@@ -119,7 +119,9 @@ class TdPropagationRetryableError(Exception):
     """Indicates that TD config hasn't propagated yet, and it's safe to retry"""
 
 
-class XdsKubernetesBaseTestCase(base_testcase.BaseTestCase):
+class XdsKubernetesBaseTestCase(
+    base_testcase.BaseTestCase
+):  # pylint: disable=too-many-public-methods
     lang_spec: TestConfig
     client_namespace: str
     client_runner: KubernetesClientRunner
