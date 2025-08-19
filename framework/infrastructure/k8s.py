7d552653e94cfd1d921a717e379669cd1df237a1
Kannan J <kannanjgithub@google.com>
2025-08-19 13:50:16 +0000

    nit mistake fix

--- framework/infrastructure/k8s.py
+++ framework/infrastructure/k8s.py
@@ -834,9 +834,10 @@ class KubernetesNamespace:  # pylint: disable=too-many-public-methods
             kind,
             self.name,
         )        
+        timeout = _timedelta(seconds=timeout_sec)
         retryer = retryers.constant_retryer(
             wait_fixed=_timedelta(seconds=wait_sec),
-            timeout=_timedelta(seconds=timeout_sec),
+            timeout=timeout,
             check_result=lambda resource: resource.metadata.annotations is not None and self.MESH_ANNOTATION in route.metadata.annotations,
         )
         try:
